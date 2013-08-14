7e835e71e9d3ab7af094f1bb25ed9849d605bfa9
Pawel Mosakowski <pawel@mosakowski.net>
2013-08-14 20:39:12 +0100

    Extended the FT to inclde a first use case.
    User visits the about page.

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,31 @@
 #! /home/mosaq/python-env/python3-django/bin/python
 
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class VisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_visitor_reads_an_about_page(self):
+        # Ramon navigates to the page and checks its title
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Homepage', self.browser.title)
+
+        self.fail('Finish the test!')
+
+        # Ramon searches for main navigation bar
+
+        # Ramon looks for a link to about page
+        # clicks it
+
+        # new page loads
+        # it displays placeholder page descritption
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
