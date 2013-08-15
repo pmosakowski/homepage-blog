265cd4a691e0207bd3c601c49902059c9cce9b4a
Pawel Mosakowski <pawel@mosakowski.net>
2013-08-15 20:45:22 +0100

    Added UT for 'About' page

--- mainpage/tests.py
+++ mainpage/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from mainpage.views import main_page
+from mainpage.views import main_page, about_page
 
 class HomePageTest(TestCase):
 
@@ -16,4 +16,15 @@ class HomePageTest(TestCase):
         response = main_page(request) 
 
         expected_html = render_to_string('base.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_about_url_resolves_to_main_page_view(self):
+        found = resolve('/about')
+        self.assertEqual(found.func, about_page)
+
+    def test_about_view_page_returns_correct_html(self):
+        request = HttpRequest() 
+        response = about_page(request) 
+
+        expected_html = render_to_string('about.html')
         self.assertEqual(response.content.decode(), expected_html)
