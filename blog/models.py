92dabd23fddd4a34858a39ebef420104f39f767c
Pawel Mosakowski <pawel@mosakowski.net>
2013-11-04 16:59:08 +0000

    Added Post model skeleton plus tests.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    pass
+
+
+# model tests go here
+from django.test import TestCase
+
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.post1 = Post()
+        self.post1.title = "A post title"
+        self.post1.content = "Some content"
+
+    def test_can_save_post_models(self):
+
+        self.assertEqual(len(Post.objects.all()), 0)
+
+        post1 = Post()
+        post1.title = "A post title"
+        post1.content = "Some content"
+        post1.save()
+
+        self.assertEqual(len(Post.objects.all()), 1)
+
+
