6c132dbf56cb387565b060ebf07894f63ce1e098
Pawel Mosakowski <pawel@mosakowski.net>
2013-11-04 20:57:09 +0000

    Added post title validation.Can't be empty now.

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,12 @@
 from django import forms
+from django.core import validators
+
+class TitleField(forms.CharField):
+    default_error_messages = {
+        'min_length': ('Title cannot be left empty.'),}
+    default_validators = [validators.MinLengthValidator(1)]
 
 class AddNewPostForm(forms.Form):
-    post_title = forms.CharField(max_length=256)
+    post_title = TitleField(max_length=512)
     post_content = forms.CharField(widget=forms.Textarea())
+        
--- blog/views.py
+++ blog/views.py
@@ -11,18 +11,11 @@ def new_post(request):
     if request.method == 'POST': 
         form = AddNewPostForm(request.POST) # A form bound to the POST data
         
-        Post.objects.create(
-                title=request.POST['post_title'],
-                content=request.POST['post_content'])
         if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-            #return render(request, 'blog/main.html', 
-            #        {'posts': 
-            #            {'title': request.POST['post_title'],
-            #             'content': form.cleaned_data['post_content']}
-            #        }
-            #)
+            Post.objects.create(
+                    title=form.cleaned_data['post_title'],
+                    content=form.cleaned_data['post_content'])
+            
             return HttpResponseRedirect('/blog') # Redirect after POST
     else:
         form = AddNewPostForm() # An unbound form
