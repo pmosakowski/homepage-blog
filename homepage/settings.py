c53f2b7b9343d99c2c6fd09278b1bfa382c9052f
Pawel Mosakowski <pawel@mosakowski.net>
2015-04-29 21:13:07 +0100

    Added top level CSS file to base mainpage template

--- homepage/settings.py
+++ homepage/settings.py
@@ -1,3 +1,9 @@
+import os.path
+
+#Get the absolute path of the settings.py file's directory
+CONFIG_DIR = os.path.dirname(os.path.realpath(__file__ )) 
+PWD = os.path.dirname(CONFIG_DIR)
+
 # Django settings for homepage project.
 
 DEBUG = True
@@ -63,7 +69,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PWD,'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
