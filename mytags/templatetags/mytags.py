92020d2f0c360192ee3578178138847dcfb653ec
Pawel Mosakowski <pawel@mosakowski.net>
2015-05-09 09:17:44 +0100

    Added humandate output filter logic for 'Today'

--- mytags/templatetags/mytags.py
+++ mytags/templatetags/mytags.py
@@ -1,6 +1,12 @@
 from django import template
+import django.utils.timezone as tz
 
 register = template.Library()
 
 def humandate(value):
-    return value
+    date = value.date()
+    today = tz.now().date()
+    if date == today:
+        return 'Today'
+    else:
+        return str(date)
--- mytags/tests.py
+++ mytags/tests.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
 
+import django.utils.timezone as tz
+import .templatetags.mytags as mt
+
 class MyTagTest(TestCase):
+    def setUp(self):
+        self.now = tz.now();
+
     def test_humandate_filter(self):
-        self.assertTrue(False)
+        self.assertEqual(mt.humandate(self.now), "Today")
