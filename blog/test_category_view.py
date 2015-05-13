420792bfd888506cd9aafcd5b4a14990c8ecc304
Pawel Mosakowski <pawel@mosakowski.net>
2015-05-13 09:23:46 +0100

    Added template for CategoryDetailView

--- blog/test_category_view.py
+++ blog/test_category_view.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 from .views import CategoryDetailView
 
 class CategoryViewTest(TestCase):
@@ -9,3 +13,12 @@ class CategoryViewTest(TestCase):
 
         self.assertEqual(CategoryDetailView.as_view().__name__, view.func.__name__)
         self.assertEqual(view2.func.__name__, view.func.__name__)
+
+    def test_url_returns_correct_html(self):
+        request = HttpRequest()
+        request.method = "POST"
+
+        response = CategoryDetailView.as_view()(request)
+        expected_html = render_to_string('blog/category_detail.html')
+
+        self.assertEqual(expected_html, response.content.decode())
